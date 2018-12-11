0e9274fcdf64f9d8d14aef36f2eb338da891f44b
Miltiadis <siavvasm@iti.gr>
2018-12-11 12:03:18 +0200

    Bug Fix - Security Issue - Tainted Input - Input Validation/Sanitization Required

--- src/main/java/miltos/vulnerableApp/App.java
+++ src/main/java/miltos/vulnerableApp/App.java
@@ -10,11 +10,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        String temp = args[0];
-        if(temp.equals("literal")){
-        	System.out.println( "Hello World!" );
-        } else {
-        	System.out.println( "Goodbye World!" );
+        String temp = args[0]; // This variable may be potentially tainted!
+        
+        if (temp != null && isClean(temp)) {
+            if(temp.equals("literal")){
+            	System.out.println( "Hello World!" );
+            } else {
+            	System.out.println( "Goodbye World!" );
+            }
         }
+
+    }
+    
+    public static boolean isClean(String userInput) {
+    	/*
+    	 * Add appropriate security checks here...
+    	 */
+    	return true;
     }
 }
