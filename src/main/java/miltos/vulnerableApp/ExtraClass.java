bf441e4de6499f2eedfa6d735670d24c34a8fee2
Miltiadis <siavvasm@iti.gr>
2018-12-11 12:20:44 +0200

    Extra methods added to ExtraClass.java

--- src/main/java/miltos/vulnerableApp/ExtraClass.java
+++ src/main/java/miltos/vulnerableApp/ExtraClass.java
@@ -8,4 +8,12 @@ public class ExtraClass {
 	public void method2(String str) {
 		System.out.println(str + " Extra2");
 	}
+	
+	public void method3(String str) {
+		System.out.println(str + "Extra");
+	}
+	
+	public void method4(String str) {
+		System.out.println(str + " Extra2");
+	}
 }
